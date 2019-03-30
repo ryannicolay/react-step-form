bb54c9b46147c1ce07483bdef4a2903fff15641e
Ryan Nicolay <ryannicolay@gmail.com>
2019-03-30 18:30:16 -0400

    Updated Success

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
-import TextField from "material-ui/TextField";
-import RaisedButton from "material-ui/RaisedButton";
 
 class Success extends Component {
-  continue = e => {
-    e.preventDefault();
-    this.props.nextStep();
-  };
-
-  state = {};
   render() {
-    const { values, handleChange } = this.props;
     return (
       <MuiThemeProvider>
         <React.Fragment>
